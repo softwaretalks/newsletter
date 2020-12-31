288af291cb8ecc998992fe5e58632c3347f2848b
Amir Hossein Baghaie <amirbagh75@gmail.com>
2020-12-31 11:07:50 +0330

    Add test list (#40)

--- configs.php
+++ configs.php
@@ -7,6 +7,10 @@ if( trim(getenv('PAKAT_API_KEY')) === '' ||
     die('Please set ENV' . PHP_EOL);
 }
 
+if(!in_array(getenv('SEND_ENV', true), ['test', 'production'])) {
+    putenv('SEND_ENV=test');
+}
+
 return [
     'REPOSITORY_ORGANIZATION'  => 'softwaretalks',
     'REPOSITORY_NAME'          => 'newsletter',
@@ -18,14 +22,15 @@ return [
     'STATE'                    => 'open',
     'EMAIL_TEMPLATE_DIR'       => './EMAIL_TEMPLATES/',
     'EMAIL_TEMPLATE_FILE_NAME' => 'newsletter.html',
-    'PAKAT_API_KEY'            => getenv('PAKAT_API_KEY'),
+    'PAKAT_API_KEY'            => getenv('PAKAT_API_KEY', true),
     'PAKAT_SMTP_HOST'          => 'smtp-relay.sendinblue.com',
     'PAKAT_SMTP_PORT'          => '587',
-    'PAKAT_SMTP_USERNAME'      => getenv('PAKAT_SMTP_USERNAME'),
-    'PAKAT_SMTP_PASSWORD'      => getenv('PAKAT_SMTP_PASSWORD'),
+    'PAKAT_SMTP_USERNAME'      => getenv('PAKAT_SMTP_USERNAME', true),
+    'PAKAT_SMTP_PASSWORD'      => getenv('PAKAT_SMTP_PASSWORD', true),
     'PAKAT_SMTP_EMAIL_NAME'    => 'Softwaretalks newsletter',
     'PAKAT_SMTP_EMAIL_ADDRESS' => 'newsletter@softwaretalks.ir',
     'PAKAT_SMTP_DEBUG'         => false,
+    'NEWSLETTER_TEST_LIST_ID'  => '8',
     'NEWSLETTER_LIST_ID'       => '2',
-    'CAN_SEND_EMAIL'           => true
+    'SEND_ENV'                 => getenv('SEND_ENV', true)
 ];
