45292275837f9848aa9dd4672881413579d97dd1
Amir Shokri <amirsh.nll@gmail.com>
2021-09-09 12:58:09 +0430

    افزودن قالب ارسال ایمیل تیره - ویژه نامه کرونا

--- src/configs.php
+++ src/configs.php
@@ -9,25 +9,28 @@ if(!in_array(getenv('SEND_ENV', true), ['test', 'production'])) {
 }
 
 return [
-    'REPOSITORY_ORGANIZATION'  => 'softwaretalks',
-    'REPOSITORY_NAME'          => 'newsletter',
-    'LABELS'                   => [
-                                    'content',
-                                    'current-week',
-                                    'verified'
-                               ],
-    'STATE'                    => 'open',
-    'EMAIL_TEMPLATE_DIR'       => __DIR__ . '/EMAIL_TEMPLATES/',
-    'EMAIL_TEMPLATE_FILE_NAME' => 'newsletter.html',
-    'PAKAT_API_KEY'            => getenv('PAKAT_API_KEY', true),
-    'PAKAT_SMTP_HOST'          => 'smtp-relay.sendinblue.com',
-    'PAKAT_SMTP_PORT'          => '587',
-    'PAKAT_EMAIL_NAME'         => 'Softwaretalks newsletter',
-    'PAKAT_EMAIL_ADDRESS'      => 'newsletter@softwaretalks.ir',
-    'PAKAT_SMTP_DEBUG'         => false,
-    'NEWSLETTER_TEST_LIST_ID'  => 8,
-    'NEWSLETTER_LIST_ID'       => 2,
-    'SEND_ENV'                 => getenv('SEND_ENV', true),
-    'TOP_CONTENT_HTML'         => getenv('TOP_CONTENT_HTML', true),
-    'BOTTOM_CONTENT_HTML'      => getenv('BOTTOM_CONTENT_HTML', true)
+    'REPOSITORY_ORGANIZATION'       => 'softwaretalks',
+    'REPOSITORY_NAME'               => 'newsletter',
+    'LABELS'                        => [
+                                        'content',
+                                        'current-week',
+                                        'verified'
+                                    ],
+    'STATE'                         => 'open',
+    'EMAIL_TEMPLATE_DIR'            => __DIR__ . '/EMAIL_TEMPLATES/',
+    'EMAIL_TEMPLATE_FILE_NAME'      => 'newsletter.html',
+    'EMAIL_TEMPLATE_DARK_FILE_NAME' => 'newsletter_dark.html',
+    'PAKAT_API_KEY'                 => getenv('PAKAT_API_KEY', true),
+    'PAKAT_SMTP_HOST'               => 'smtp-relay.sendinblue.com',
+    'PAKAT_SMTP_PORT'               => '587',
+    'PAKAT_EMAIL_NAME'              => 'Softwaretalks newsletter',
+    'PAKAT_EMAIL_ADDRESS'           => 'newsletter@softwaretalks.ir',
+    'PAKAT_SMTP_DEBUG'              => false,
+    'NEWSLETTER_TEST_LIST_ID'       => 8,
+    'NEWSLETTER_LIST_ID'            => 2,
+    'SEND_ENV'                      => getenv('SEND_ENV', true),
+    'TOP_CONTENT_HTML'              => getenv('TOP_CONTENT_HTML', true),
+    'BOTTOM_CONTENT_HTML'           => getenv('BOTTOM_CONTENT_HTML', true),
+    'BOTTOM_CONTENT_HTML_DARK'      => getenv('BOTTOM_CONTENT_HTML_DARK', true),
+    'IS_DARK'                       => getenv('IS_DARK', true)
 ];
