9f2baaafcda419e6f1c68b160f676c0eb6df13ec
Amirhossein Baghaie <ohmydevops@gmail.com>
2021-11-07 01:51:04 +0330

    --

--- test.sh
+++ test.sh
@@ -11,3 +11,9 @@ docker run --rm --init newsletter-test -v
 echo -e -n "\n--- Test 1: Get contributors section ---\n\n"
 docker run --rm --init newsletter-test src/contributors_statistics.php
 
+# check is valid or not?
+# test2: check valid content created or not? (html)
+
+echo -e -n "\n--- Test 2: Get HTML export ---\n\n"
+# docker run --rm --init newsletter-test src/contributors_statistics.php
+
