983282c8db56b2e809451f566e064e23e53f669c
Amirhossein Baghaie <ohmydevops@gmail.com>
2022-06-16 09:03:12 +0430

    Update counter.php

--- src/modules/counter.php
+++ src/modules/counter.php
@@ -8,7 +8,7 @@ function newsletterCounter(): int
     // This is our first posting date. (number 1)
     $newsletterStartDate = $now->createFromDate(2021, 01, 02); 
     // minus 6, because we did not send the newsletter for 4 weeks in Farvardin 1400 & 2 week in Mordad 1400.
-    $newsletterNumber = (int)(($newsletterStartDate->diffInWeeks($now)) + 1) - 18;
+    $newsletterNumber = (int)(($newsletterStartDate->diffInWeeks($now)) + 1) - 23;
 
     return $newsletterNumber;
 }
