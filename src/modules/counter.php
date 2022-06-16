f7101502aeb9d3a22785aa0d26b8502111e4bb7f
Amirhossein Baghaie <ohmydevops@gmail.com>
2022-06-16 09:04:49 +0430

    --

--- src/modules/counter.php
+++ src/modules/counter.php
@@ -7,7 +7,7 @@ function newsletterCounter(): int
     $now = Carbon::now();
     // This is our first posting date. (number 1)
     $newsletterStartDate = $now->createFromDate(2021, 01, 02); 
-    // minus 6, because we did not send the newsletter for 4 weeks in Farvardin 1400 & 2 week in Mordad 1400.
+    // minus 23, because we did not send the newsletter for 23 weeks
     $newsletterNumber = (int)(($newsletterStartDate->diffInWeeks($now)) + 1) - 23;
 
     return $newsletterNumber;
